63efb5229025f5f2c35cb534520c6fa17a52ad8f
temphJ <temphi20@gmail.com>
2021-04-12 19:44:17 +0900

    browserify/watchify 오류 안나게 수정

--- app/app.js
+++ app/app.js
@@ -3,15 +3,29 @@
 const express = require("express");
 const bparser = require("body-parser");
 const dotenv = require("dotenv");
-// const fetch = require("isomorphic-fetch");
-// const fetch = require("node-fetch");
-// const d3 = require("d3");
+// const browserify = require("browerify");
+
+// const b = browserify({
+//     entries: ["./app/bin/www.js"],
+//     cache: {},
+//     packageCache: {},
+//     plugin: [watchify]
+// })
+// b.on("update", bundle);
+// bundle();
+
+// function bundle(){
+//     b.bundle()
+//         .on("error", console.error)
+//         .pipe()
+// }
 
 const app = express();
 dotenv.config();
 
 // router
 const home = require("./src/routes/home");
+// const { default: bundle } = require("../bundle");
 
 // app setting
 app.set("views", "./app/src/views");
